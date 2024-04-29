5682539428e9fdf2280dc22331db5249b4ec8d58
Harsh Kamde <105597593+harsh-kamde@users.noreply.github.com>
2024-04-29 21:32:21 +0530

    Update app.py
    specialist added

--- app.py
+++ app.py
@@ -20,6 +20,49 @@ diets = pd.read_csv("datasets/diets.csv")
 # load model===========================================
 svc = pickle.load(open('models/svc.pkl','rb'))
 
+disease_specialist = {
+    "Allergy": "Allergist",
+    "Drug Reaction": "Allergist",
+    "Hypertension": "Cardiologist",
+    "Heart attack": "Cardiologist",
+    "Psoriasis": "Dermatologist",
+    "Chicken pox": "Dermatologist",
+    "Acne": "Dermatologist",
+    "Impetigo": "Dermatologist",
+    "Fungal infection": "Dermatologist",
+    "Hypothyroidism": "Endocrinologist",
+    "Diabetes": "Endocrinologist",
+    "Hypoglycemia": "Endocrinologist",
+    "Hyperthyroidism": "Endocrinologist",
+    "GERD": "Gastroenterologist",
+    "Peptic ulcer disease": "Gastroenterologist",
+    "Jaundice": "Gastroenterologist",
+    "Dimorphic hemorrhoids (piles)": "Gastroenterologist",
+    "Gastroenteritis": "Gastroenterologist",
+    "Urinary tract infection": "Gynecologist",
+    "Chronic cholestasis": "Hepatologist",
+    "Hepatitis A": "Hepatologist",
+    "Hepatitis B": "Hepatologist",
+    "Hepatitis C": "Hepatologist",
+    "Hepatitis E": "Hepatologist",
+    "Hepatitis D": "Hepatologist",
+    "Alcoholic hepatitis": "Hepatologist",
+    "Malaria": "Internal Medicine",
+    "Dengue": "Internal Medicine",
+    "Migraine": "Neurologist",
+    "Cervical spondylosis": "Neurologist",
+    "Paralysis (brain hemorrhage)": "Neurologist",
+    "AIDS": "Osteopathic",
+    "Paroxysmal Positional Vertigo (vertigo)": "Otolaryngologist",
+    "Common Cold": "Otolaryngologist",
+    "Typhoid": "Pediatrician",
+    "Varicose veins": "Phlebologist",
+    "Bronchial Asthma": "Pulmonologist",
+    "Pneumonia": "Pulmonologist",
+    "Osteoarthritis": "Rheumatologist",
+    "Arthritis": "Rheumatologist",
+    "Tuberculosis": "Tuberculosis Specialist",
+}
 
 #============================================================
 # custome and helping functions
@@ -45,7 +88,7 @@ def helper(dis):
     wrkout_series = workout[workout['disease'] == dis]['workout']
     wrkout = wrkout_series.tolist()  # Convert Series to list
     
-    return desc, pre, med, die, wrkout
+    return desc, pre, med, die, wrkout, specialist
 
 
 symptoms_dict = {'itching': 0, 'skin_rash': 1, 'nodal_skin_eruptions': 2, 'continuous_sneezing': 3, 'shivering': 4, 'chills': 5, 'joint_pain': 6, 'stomach_pain': 7, 'acidity': 8, 'ulcers_on_tongue': 9, 'muscle_wasting': 10, 'vomiting': 11, 'burning_micturition': 12, 'spotting_ urination': 13, 'fatigue': 14, 'weight_gain': 15, 'anxiety': 16, 'cold_hands_and_feets': 17, 'mood_swings': 18, 'weight_loss': 19, 'restlessness': 20, 'lethargy': 21, 'patches_in_throat': 22, 'irregular_sugar_level': 23, 'cough': 24, 'high_fever': 25, 'sunken_eyes': 26, 'breathlessness': 27, 'sweating': 28, 'dehydration': 29, 'indigestion': 30, 'headache': 31, 'yellowish_skin': 32, 'dark_urine': 33, 'nausea': 34, 'loss_of_appetite': 35, 'pain_behind_the_eyes': 36, 'back_pain': 37, 'constipation': 38, 'abdominal_pain': 39, 'diarrhoea': 40, 'mild_fever': 41, 'yellow_urine': 42, 'yellowing_of_eyes': 43, 'acute_liver_failure': 44, 'fluid_overload': 45, 'swelling_of_stomach': 46, 'swelled_lymph_nodes': 47, 'malaise': 48, 'blurred_and_distorted_vision': 49, 'phlegm': 50, 'throat_irritation': 51, 'redness_of_eyes': 52, 'sinus_pressure': 53, 'runny_nose': 54, 'congestion': 55, 'chest_pain': 56, 'weakness_in_limbs': 57, 'fast_heart_rate': 58, 'pain_during_bowel_movements': 59, 'pain_in_anal_region': 60, 'bloody_stool': 61, 'irritation_in_anus': 62, 'neck_pain': 63, 'dizziness': 64, 'cramps': 65, 'bruising': 66, 'obesity': 67, 'swollen_legs': 68, 'swollen_blood_vessels': 69, 'puffy_face_and_eyes': 70, 'enlarged_thyroid': 71, 'brittle_nails': 72, 'swollen_extremeties': 73, 'excessive_hunger': 74, 'extra_marital_contacts': 75, 'drying_and_tingling_lips': 76, 'slurred_speech': 77, 'knee_pain': 78, 'hip_joint_pain': 79, 'muscle_weakness': 80, 'stiff_neck': 81, 'swelling_joints': 82, 'movement_stiffness': 83, 'spinning_movements': 84, 'loss_of_balance': 85, 'unsteadiness': 86, 'weakness_of_one_body_side': 87, 'loss_of_smell': 88, 'bladder_discomfort': 89, 'foul_smell_of urine': 90, 'continuous_feel_of_urine': 91, 'passage_of_gases': 92, 'internal_itching': 93, 'toxic_look_(typhos)': 94, 'depression': 95, 'irritability': 96, 'muscle_pain': 97, 'altered_sensorium': 98, 'red_spots_over_body': 99, 'belly_pain': 100, 'abnormal_menstruation': 101, 'dischromic _patches': 102, 'watering_from_eyes': 103, 'increased_appetite': 104, 'polyuria': 105, 'family_history': 106, 'mucoid_sputum': 107, 'rusty_sputum': 108, 'lack_of_concentration': 109, 'visual_disturbances': 110, 'receiving_blood_transfusion': 111, 'receiving_unsterile_injections': 112, 'coma': 113, 'stomach_bleeding': 114, 'distention_of_abdomen': 115, 'history_of_alcohol_consumption': 116, 'fluid_overload.1': 117, 'blood_in_sputum': 118, 'prominent_veins_on_calf': 119, 'palpitations': 120, 'painful_walking': 121, 'pus_filled_pimples': 122, 'blackheads': 123, 'scurring': 124, 'skin_peeling': 125, 'silver_like_dusting': 126, 'small_dents_in_nails': 127, 'inflammatory_nails': 128, 'blister': 129, 'red_sore_around_nose': 130, 'yellow_crust_ooze': 131}
@@ -78,10 +121,11 @@ def home():
     if not predicted_disease:
         return jsonify({'message': 'No prediction could be made for the given symptoms'}), 404
 
-    dis_des, pre, med, die, wrkout = helper(predicted_disease)
+    dis_des, pre, med, die, wrkout, specialist = helper(predicted_disease)
 
     response = {
         'predicted_disease': predicted_disease,
+        'specialist': specialist,
         'dis_des': dis_des,
         'precautions': pre,  # Adjusted from 'my_precautions'
         'medications': med,
